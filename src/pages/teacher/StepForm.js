ffbaad9e28aaaa3ce5d720adc0ab1c9930537f73
MayankTomar <mayank.ctinfotech@gmail.com>
2024-08-27 12:15:11 +0530

    changes

--- src/pages/teacher/StepForm.js
+++ src/pages/teacher/StepForm.js
@@ -1,12 +1,11 @@
-import React, { useState } from "react";
+import React from "react";
 import FirstStep from "../../components/Steps/FirstStep";
 import SecondStep from "../../components/Steps/SecondStep";
 import ThirdStep from "../../components/Steps/ThirdStep";
-import { useDispatch, useSelector } from "react-redux";
+import { useSelector } from "react-redux";
 import FourthStep from "../../components/Steps/FourthStep";
 
 const StepForm = () => {
-    const dispatch = useDispatch();
   const { currentStep } = useSelector((state) => state?.authReducer);
   const CurrentPage = (pageNo) => {
     switch (pageNo) {
@@ -37,10 +36,14 @@ const StepForm = () => {
             <form id="msform" action="dashboard.html">
               {/* <!-- progressbar --> */}
               <ul id="progressbar" class="mb-5">
-                <li class={currentStep >= 1 ? "active" : ""}>Step 1</li>
-                <li class={currentStep >= 2 ? "active" : ""}>Step 2</li>
-                <li class={currentStep >= 3 ? "active" : ""}>Step 3</li>
-                <li class={currentStep >= 4 ? "active" : ""}>Step 4</li>
+                {[1, 2, 3, 4]?.map((item, index) => (
+                  <li
+                    key={index}
+                    class={currentStep >= index + 1 ? "active" : ""}
+                  >
+                    Step {index + 1}
+                  </li>
+                ))}
               </ul>
               {/* <!-- fieldsets --> */}
               <div class="ct_step_form_inner">{CurrentPage(currentStep)}</div>
